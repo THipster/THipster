96717287f60252c5d2bfb2103f55435e2aea1a61
rcattin <rafa.cattin@gmail.com>
2023-04-20 15:16:39 +0200

    ✨ feat: add file reader in parser

---
+++ tests/parser/dsl_parser/test_DSLparser.py
@@ -0,0 +1,50 @@
+from parser.dsl_parser.DSLParser import DSLParser
+import os
+
+
+def create_dir(dirname: str, files: dict[str, str]):
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+
+    dirname = os.path.abspath(dirname)
+    for name, content in files.items():
+        create_file(dirname, name, content)
+
+    def destroy_files():
+        for content in os.listdir(dirname):
+            os.remove(f'{dirname}/{content}')
+        os.rmdir(dirname)
+
+    return destroy_files
+
+
+def create_file(filename: str, content: str, dirname: str = 'test'):
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+
+    dirname = os.path.abspath(dirname)
+
+    file = open(f'{dirname}/{filename}', 'w')
+    file.write(content)
+    file.close()
+
+
+def test_get_files():
+    path_input = 'test'
+    _destroy_dir = create_dir(
+        path_input,
+        {f'test_file{i}.thips': 'content' for i in range(1, 4)},
+    )
+
+    parser = DSLParser()
+
+    files = parser._DSLParser__getfiles(path_input)
+
+    assert isinstance(files, dict)
+    assert len(files) == 3
+
+    for k, v in files.items():
+        assert 'test_file' in k
+        assert v == 'content'
+
+    _destroy_dir()
--- thipster/parser/dsl_parser/DSLParser.py
+++ thipster/parser/dsl_parser/DSLParser.py
@@ -1,12 +1,55 @@
 from engine.I_Parser import I_Parser
 from engine.ParsedFile import ParsedFile
 
+import os
+
+
+class DSLParserBaseException(Exception):
+    def __init__(self, message, *args: object) -> None:
+        super().__init__(*args)
+
+        self.__message = message
+
+    @property
+    def message(self):
+        return self.__message
+
 
 class DSLParser(I_Parser):
-    def run(self) -> ParsedFile:
-        # files = ???
 
-        # lexer = Lexer(files)
-        # token_list = lexer.run()
-        #
+    def __getfiles(self, path: str) -> dict[str, str]:
+
+        path = os.path.abspath(path)
+
+        if not os.path.exists(path):
+            raise DSLParserBaseException(f'Path not found : {path}')
+
+        files = {}
+
+        if os.path.isdir(path):
+            for content in os.listdir(path):
+                files.update(self.__getfiles(f'{path}/{content}'))
+
+        if os.path.isfile(path):
+            with open(path, 'r') as f:
+                files.update({path: f.read()})
+
+                f.close()
+
+        return files
+
+    def run(self, path: str) -> ParsedFile:
+
+        try:
+            files = self.__getfiles(path)
+            print(files)
+            # lexer = Lexer(files)
+            # token_list = lexer.run()
+            #
+        except DSLParserBaseException as e:
+            print(e.message)
+            pass
+        except Exception:
+            pass
+
         pass
