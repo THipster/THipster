0cbe799dfc232c9381182af17a92786511314582
rcattin <rafa.cattin@gmail.com>
2023-04-20 12:06:42 +0200

    💩 feat: add DSLParser strycture

---
+++ thipster/parser/dsl_parser/DSLParser.py
@@ -0,0 +1,12 @@
+from engine.I_Parser import I_Parser
+from engine.ParsedFile import ParsedFile
+
+
+class DSLParser(I_Parser):
+    def run(self) -> ParsedFile:
+        # files = ???
+
+        # lexer = Lexer(files)
+        # token_list = lexer.run()
+        #
+        pass
