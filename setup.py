d623963b1f4ed1ed293cee73474a74a3637e50d4
rcattin <rafa.cattin@gmail.com>
2023-04-21 15:55:35 +0200

    :wrench: fix: requirements.

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup
 
 __version__ = '0.0.1'
 
-requirements = [
-    '',
-]
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='thipster',
     version=__version__,
     package_dir={'': 'thipster'},
-    install_requires=requirements,
+    install_requires=required,
     extras_require={
         'test': [
             'pytest',
