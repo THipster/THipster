57746f9b19b1cf2159f75e99eaa2ea85b6296520
rcattin <rafa.cattin@gmail.com>
2023-05-03 15:15:29 +0200

    :bug: fix: update gh repo

--- thipster/demo.py
+++ thipster/demo.py
@@ -1,21 +1,15 @@
 import engine.Engine as eng
 from parser.dsl_parser.DSLParser import DSLParser
-from engine.ResourceModel import ResourceModel
 from helpers import logger
 
 from parser.dsl_parser.DSLParser import DSLParserPathNotFound
 from parser.dsl_parser.TokenParser import DSLSyntaxException
+from repository.GithubRepo import GithubRepo
 
 
 class MockAuth(eng.I_Auth):
     @logger('- Authentifier')
     def run(self):
-        pass
-
-
-class MockRepository(eng.I_Repository):
-    @logger('- Repo')
-    def run(self) -> list[ResourceModel]:
         pass
 
 
@@ -28,7 +22,7 @@ class MockTerraform(eng.I_Terraform):
 def demo():
     file = input()
     engine = eng.Engine(
-        DSLParser(), MockRepository(),
+        DSLParser(), GithubRepo('Thipster/models'),
         MockAuth(), MockTerraform(),
     )
 
