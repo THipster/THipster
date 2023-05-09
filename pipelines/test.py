af0f6b9cfb6e48b2b33fafc16e4e044cdb7c4780
Gabriel Suquet <gsuquet@ippon.fr>
2023-05-09 12:02:14 +0200

    :memo: fix(ci): Add doc to dagger pipelines

--- pipelines/test.py
+++ pipelines/test.py
@@ -6,6 +6,8 @@ import dagger
 
 
 async def test():
+    """Runs all the automated tests
+    """
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
         python = (
             base.pythonBase(client, '3.11')
@@ -17,6 +19,25 @@ async def test():
         await python.exit_code()
 
     print('Tests succeeded!')
+
+
+def add_all_tests_step(precedent_container: dagger.Container) -> dagger.Container:
+    """Adds a step to a dagger pipeline container to run all the automated tests
+
+    Parameters
+    ----------
+    precedent_container : Container
+        The container to add the step to
+
+    Returns
+    -------
+    Container
+        Returns a container with the step to run all the automated tests
+    """
+    tests_step = precedent_container.with_exec(
+        ['pip', 'install', '-e', '.[test]'],
+    ).with_exec(['pytest', 'tests'])
+    return tests_step
 
 
 if __name__ == '__main__':
