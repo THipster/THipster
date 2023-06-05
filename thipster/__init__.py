b38e0dc927fadedfa45573c57ade806c24a1ff7e
Gabriel Suquet <gsuquet@ippon.fr>
2023-06-05 13:20:55 +0200

    :memo: fix: vocab and syntax

--- thipster/__init__.py
+++ thipster/__init__.py
@@ -1,4 +1,4 @@
-"""THipster is a tool dedicated to simplifying the ordeal associated with writing \
+"""THipster is a tool dedicated to simplifying the difficulty associated with writing \
 Terraform files. 
 
 It allows users to write infrastructure as code in a simplified format, using either 
--- thipster/repository/__init__.py
+++ thipster/repository/__init__.py
@@ -2,7 +2,7 @@
 
 Contains the various options to store the JSON models used by THipster to generate the 
 Python Terraform CDK constructs.
-Currently, two options are available :
+Currently, two options are available:
 - A local repository, which stores the models in a local folder
 - A GitHub repository
 """
