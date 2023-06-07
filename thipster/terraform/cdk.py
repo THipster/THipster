8ab3b7e62907b8992f3ae54e5f7a29c40a7cba77
rcattin <rafa.cattin@gmail.com>
2023-06-07 10:41:03 +0200

    :memo: docs: cdk

--- thipster/terraform/cdk.py
+++ thipster/terraform/cdk.py
@@ -26,13 +26,12 @@ class CDK(I_Terraform):
     _logger = Logger(__name__)
 
     def apply(self):
-        """Apply generated terraform code
+        """Applies generated Terraform plan
 
-        Raises
-        ------
-        NotImplementedError
-            If method is not implemented in inheriting classes
-
+        Returns
+        -------
+        str
+            Terraform apply output
         """
         t = Terraform()
         _, stdout, stderr = t.apply()
@@ -42,6 +41,17 @@ class CDK(I_Terraform):
         self, file: pf.ParsedFile, models: dict[str, rm.ResourceModel],
         _auth: I_Auth,
     ):
+        """Generate Terraform file from given parsed file and models
+
+        Parameters
+        ----------
+        file : pf.ParsedFile
+            parsedFile object to transform
+        models : dict[str, rm.ResourceModel]
+            associated models
+        _auth : I_Auth
+            authenticator to use
+        """
         CDK._created_resources = {}
         CDK._models = models
         CDK._parent_resources_stack = []
@@ -96,13 +106,12 @@ class CDK(I_Terraform):
         os.rmdir('cdktf.out')
 
     def init(self):
-        """Get plan from generated terraform code
+        """Initilize terraform
 
-        Raises
-        ------
-        NotImplementedError
-            If method is not implemented in inheriting classes
-
+        Returns
+        -------
+        str
+            Terraform init output
         """
         t = Terraform()
         _, stdout, stderr = t.init()
@@ -111,17 +120,23 @@ class CDK(I_Terraform):
     def plan(self):
         """Get plan from generated terraform code
 
-        Raises
-        ------
-        NotImplementedError
-            If method is not implemented in inheriting classes
-
+        Returns
+        -------
+        str
+            Terraform plan output
         """
         t = Terraform()
         _, stdout, stderr = t.plan(out="thipster.tfplan")
         return stdout + stderr
 
     def _pip_install(package: str):
+        """Install a package if it wasn't already installed by thipster
+
+        Parameters
+        ----------
+        package : str
+            package to install
+        """
         if package not in CDK._imported_packages:
             subprocess.check_call(
                 [sys.executable, '-m', 'pip', 'install', '-qqq', package],
@@ -129,7 +144,22 @@ class CDK(I_Terraform):
             CDK._imported_packages.append(package)
 
     def _import(package_name: str, module_name: str, class_name: str) -> type:
+        """Import a class from any package
 
+        Parameters
+        ----------
+        package_name : str
+            package that contains the class
+        module_name : str
+            module that contains the class
+        class_name : str
+            class to import
+
+        Returns
+        -------
+        type :
+            the imported class
+        """
         module = importlib.import_module(f'{package_name}.{module_name}')
         class_ = getattr(module, class_name)
 
@@ -139,6 +169,27 @@ class CDK(I_Terraform):
         self, resource_type: str, parent_name: str | None = None,
         no_modif: bool = True, no_dependencies: bool = False,
     ):
+        """Create a resource with all default values
+
+        Parameters
+        ----------
+        self : _ResourceStack
+            Terraform CDK stack that contains the resource
+        resource_type : str
+            type of the resource to create
+        parent_name : str, optional
+            name of its parent, default None
+        no_modif : bool, optional
+            if True, raise errors if resource can't be created with default values, \
+default False
+        no_dependencies : bool, optional
+            if True, create the default dependencies, default False
+
+        Returns
+        -------
+        (type, str, dict[str, object]) :
+            all the information needed to instantiate the class with default values
+        """
         # Checks for cyclic dependency
         if resource_type in CDK._parent_resources_stack:
             CDK._logger.error(
@@ -185,6 +236,22 @@ class CDK(I_Terraform):
         return (resource_class, name, resource_args)
 
     def _create_resource(self, resource_args: object, resource_type: str = None):
+        """Create a resource with the given values
+
+        Parameters
+        ----------
+        self : _ResourceStack
+            Terraform CDK stack that contains the resource
+        resource_args : object
+            data source to create the resource
+        resource_type : str, optional
+            type of the resource to create, default None
+
+        Returns
+        -------
+        object : 
+            the created resource
+        """
         if isinstance(resource_args, dict):
             return CDK._create_resource_from_dict(self, resource_type, resource_args)
 
@@ -196,7 +263,22 @@ class CDK(I_Terraform):
     def _create_resource_from_args(
         self, resource_type: str, input_args: list[pf.ParsedAttribute] | None,
     ):
+        """Create a resource from a list of ParsedAttributes
 
+        Parameters
+        ----------
+        self : _ResourceStack
+            Terraform CDK stack that contains the resource
+        resource_type : str, optional
+            type of the resource to create, default None
+        input_args : list[ParsedAttribute]
+            data source to create the resource
+
+        Returns
+        -------
+        object : 
+            the created resource
+        """
         resource_class, resource_name, resource_args = CDK._create_default_resource(
             self,
             resource_type,
@@ -211,7 +293,7 @@ class CDK(I_Terraform):
             if attribute.name and model.name_key:
                 resource_args[model.name_key] = attribute.name
 
-            resource_args, dependencies = CDK._process_attribute(
+            CDK._process_attribute(
                 self, model, attribute, resource_args, dependencies,
             )
 
@@ -234,6 +316,22 @@ class CDK(I_Terraform):
     def _create_resource_from_dict(
         self, resource_type: str, input_args: dict | None,
     ):
+        """Create a resource from a dictionnary
+
+        Parameters
+        ----------
+        self : _ResourceStack
+            Terraform CDK stack that contains the resource
+        resource_type : str, optional
+            type of the resource to create, default None
+        input_args : dict
+            data source to create the resource
+
+        Returns
+        -------
+        object : 
+            the created resource
+        """
         resource_class, resource_name, resource_args = CDK._create_default_resource(
             self,
             resource_type,
@@ -248,7 +346,7 @@ class CDK(I_Terraform):
             if name and model.name_key:
                 resource_args[model.name_key] = name
 
-            resource_args, dependencies = CDK._process_attribute(
+            CDK._process_attribute(
                 self, model,
                 pf.ParsedAttribute(
                     name, None, pf.ParsedLiteral(value),
@@ -280,6 +378,20 @@ class CDK(I_Terraform):
             no_modif=False,
             no_dependencies=True,
         )
+        """Create a resource from a list of ParsedAttributes
+
+        Parameters
+        ----------
+        self : _ResourceStack
+            Terraform CDK stack that contains the resource
+        resource : ParsedResource
+            data source to create the resource
+
+        Returns
+        -------
+        object : 
+            the created resource
+        """
         model = CDK._models[resource.type]
 
         if model.name_key:
@@ -288,7 +400,7 @@ class CDK(I_Terraform):
         # Process attributes
         dependencies = copy.deepcopy(model.dependencies)
         for attribute in resource.attributes:
-            resource_args, dependencies = CDK._process_attribute(
+            CDK._process_attribute(
                 self,
                 model,
                 attribute,
@@ -317,6 +429,21 @@ class CDK(I_Terraform):
         resource_args: dict[str, object],
         dependencies: dict[str, dict[str, object]] | None,
     ):
+        """Process an attribute
+
+        Parameters
+        ----------
+        self : _ResourceStack
+            Terraform CDK stack that contains the resource
+        model : ResourceModel
+            model of the resource that is being created
+        attribute : ParsedAttribute
+            attribute to handle
+        resource_args : object
+            data source needed to create the resource
+        dependencies : dict[str, dict[str, object]]
+            dependencies needed to create the resource
+        """
         if attribute.name in dependencies:
             created_name = f"{dependencies[attribute.name]['resource']}/"\
                            f"{attribute.value}"
@@ -338,18 +465,18 @@ class CDK(I_Terraform):
             resource_args[attribute.name] = CDK._created_resources[created_name]
 
             del dependencies[attribute.name]
-            return resource_args, dependencies
+            return
 
         # Checks if attribute is an internal object
         if attribute.name in model.internal_objects:
             resource_args = CDK._create_internal_object(
                 self,
-                model.internal_objects[attribute.name],
                 attribute.name,
+                model.internal_objects[attribute.name],
                 attribute.value,
                 resource_args,
             )
-            return resource_args, dependencies
+            return
 
         # Checks if attribute is expected as an attibute
         if attribute.name not in model.attributes:
@@ -368,19 +495,32 @@ class CDK(I_Terraform):
         # Sets attribute value
         resource_args[model.attributes[attribute.name].cdk_name] = attribute_value
 
-        return resource_args, dependencies
-
     def _generate_default_dependencies(
             self,
             dependencies,
             model: rm.ResourceModel,
             resource_args,
-            resourceName,
+            resource_name,
     ):
+        """Generate default dependencies and internal objects in a resource
+
+        Parameters
+        ----------
+        self : _ResourceStack
+            Terraform CDK stack that contains the resource
+        dependencies : 
+            list of dependencies to create
+        model : str
+            model of the resource that is being created
+        resource_args : _type_
+            data source needed to create the resource
+        resource_name : dict
+            name of the resource that is being created
+        """
         # Create missing dependencies
         for dependency_name, dependency_object in dependencies.items():
             CDK._create_dependency(
-                self, dependency_name, dependency_object, resource_args, resourceName,
+                self, dependency_name, dependency_object, resource_args, resource_name,
             )
 
         # Create default internal objects if needed
@@ -389,19 +529,34 @@ class CDK(I_Terraform):
                 for default_args in internal_object['defaults']:
                     CDK._create_internal_object(
                         self,
-                        model.internal_objects[internal_object_name],
                         internal_object_name,
+                        model.internal_objects[internal_object_name],
                         default_args,
                         resource_args,
                     )
 
     def _create_internal_object(
         self,
-        internal_object_model,
         name: str,
+        internal_object_model,
         internal_object_args,
         resource_args: dict,
     ):
+        """Create an internal object in a resource
+
+        Parameters
+        ----------
+        self : _ResourceStack
+            Terraform CDK stack that contains the resource
+        name : str
+            name of the internal object
+        internal_object_model : ResourceModel
+            model of the internal object
+        internal_object_args : object
+            data source to create the internal object
+        resource_args : dict
+            data source needed to create the resource
+        """
         internal_object_type = internal_object_model['var_type'] \
             if 'var_type' in internal_object_model else 'Unknown'
 
@@ -420,7 +575,7 @@ class CDK(I_Terraform):
                     )
                     resource_args[name] += [res]
 
-                return resource_args
+                return
 
             res = CDK._create_resource(
                 self,
@@ -428,7 +583,7 @@ class CDK(I_Terraform):
                 internal_object_model['resource'],
             )
             resource_args[name] += [res]
-            return resource_args
+            return
 
         res = CDK._create_resource(
             self,
@@ -437,9 +592,24 @@ class CDK(I_Terraform):
         )
         resource_args[name] = res
 
-        return resource_args
+        return
 
     def _create_dependency(self, dep_name, dep_value, resource_args, parent_name):
+        """Create an internal object in a resource
+
+        Parameters
+        ----------
+        self : _ResourceStack
+            Terraform CDK stack that contains the resource
+        dep_name : str
+            name of the dependency
+        dep_value : ResourceModel
+            model of the dependency
+        resource_args : dict
+            data source needed to create the resource
+        parent_name : str
+            name of its parent
+        """
         class_, class_name, class_attributes = CDK._create_default_resource(
             self, dep_value['resource'], parent_name,
         )
