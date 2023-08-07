2cf743b1dc77e5a0f6fc9c64a39cde2476283435
rcattin <rafa.cattin@gmail.com>
2023-08-07 18:10:14 +0200

    :recycle: fix: use engine properties

--- thipster/engine/exceptions.py
+++ thipster/engine/exceptions.py
@@ -14,3 +14,27 @@ class THipsterError(Exception, ABC):
     def __str__(self) -> str:
         """Get the string representation of the exception."""
         return self.message
+
+
+class BadPortError(THipsterError):
+    """Exception raised when trying to set a port with an invalid class."""
+
+    def __init__(self, got: type, expected: type, *args: object) -> None:
+        """Exception raised when trying to set a port with an invalid class.
+
+        Parameters
+        ----------
+        var : str
+            The variable name.
+        """
+        super().__init__(*args)
+        self.got = got
+        self.expected = expected
+
+    @property
+    def message(self):
+        """Get the message of the exception."""
+        return (
+            f'Error while setting THipster port : got {self.got.__name__},'
+            f' expected {self.expected.__name__}'
+        )
